6e1f138234aa8ddc9d841c2bd868672b4988c267
Ciaran Maguire <maguireci@gmail.com>
2015-05-09 12:25:08 +0700

    cleaned up code, fixed some bugs in js, updated todo

--- myscript.js
+++ myscript.js
@@ -15,8 +15,7 @@ var extListTemplate = Handlebars.compile(extListSource);
 
 $(document).ready(function(){
 
-	// call function to check storage.sync for existing user profiles:
-	getProfiles();
+	getProfiles(); // call function to check storage.sync for existing user profiles
 
 	$('.modal-trigger').leanModal();
 
@@ -43,9 +42,8 @@ $(document).ready(function(){
 			}
 			entry.pic = imgsrc; // setting the url we got earlier as entry.pic
 
-			// setting switches to either on or off
 			var state = entry.enabled;
-			if(state === true){
+			if(state === true){ // set switches to either on or off
 				state = "checked"
 			} else {
 				state = ""
@@ -58,50 +56,35 @@ $(document).ready(function(){
 			} else {
 				$('#inactiveExtensions').append(template(entry));
 			}
-		});
+		}); // close extArray.forEach
 
 		extStateListener(); // run the function which listens for a change in a checkbox state
 
-
-
-
-
-
-
-chrome.storage.sync.get(function(obj){
-	// setting the profile buttons to on/off appearance
-	var sizeOfStoredObject = Object.keys(obj).length;
-	// console.log(sizeOfStoredObject);
-	console.log(sizeOfStoredObject);
-	for (var n = 0; n < sizeOfStoredObject; n++) { // cycle through the profilesHolder
-		// console.log(Object.keys(obj)[i])
-		for (key in obj) { // cycle through the extension profiles
-			$("#" + key).addClass("on").removeClass("off");
-			for (var n = 0; n < obj[key].length; n++) { // cycle through the extensions within the profile
-				for (i=0;i<extArray.length;i++) {
-					// console.log("this is the extension array with obj[key]["+n+"] ---" +obj[key][n]);
-					if (extArray[i]["id"] === obj[key][n]) {
-						if (extArray[i]["enabled"] === false) {
-							$("#" + key).removeClass("on").addClass("off");
+		chrome.storage.sync.get(function(obj){
+			// setting the profile buttons to on/off appearance
+			var sizeOfStoredObject = Object.keys(obj).length;
+			for (var n = 0; n < sizeOfStoredObject; n++) { // cycle through the profilesHolder
+				// console.log(Object.keys(obj)[i])
+				for (key in obj) { // cycle through the extension profiles
+					$("#" + key).addClass("on").removeClass("off");
+					for (var n = 0; n < obj[key].length; n++) { // cycle through the extensions within the profile
+						for (i=0;i<extArray.length;i++) {
+							// console.log("this is the extension array with obj[key]["+n+"] ---" +obj[key][n]);
+							if (extArray[i]["id"] === obj[key][n]) {
+								if (extArray[i]["enabled"] === false) {
+									$("#" + key).removeClass("on").addClass("off");
+								}
+							}
 						}
 					}
 				}
-
-
-				// THIS NEXT SECTION NEEDS TO TAKE EACH EXTENSION ID FROM THE PROFILE'S ARRAY LIKE SO obj[key][i] AND FIND OUT WHETHER THE EXTENSION IS TURNED ON OR NOT THEN IF ANY OF THE EXTENSIONS ARE TURNED OFF THEN APPLY THE "off" CLASS TO THE BTN
-
-
 			}
-
-		}
-	}
-}) // close sync.get
-
+		}) // close chrome.storage.sync.get
 
 		$(".extId").hide(); // just here to reference each individual ext
 		$(".extState").hide(); // just here to reference each individual ext's state
 
-	});
+	}); // close chrome.management.getAll
 
 	// Search
 	$("#searchbox").keyup(function(){
@@ -121,25 +104,24 @@ chrome.storage.sync.get(function(obj){
 
 }); // close $(document).ready
 
-
 // after clicking a profile button toggle it's appearance (on or off) and cycle through associated extensions turning them all on or off
 $("body").on("click",".profile-btn",function(){ // if a profile btn is clicked
 	btnId = $(this).attr("id"); // find out which one and assign to btnId
 	btnIdConvert = btnId.split('_').join(' '); // lower case and replace underscore for space
+
 	if ($(this).hasClass("on")) { // if the btn is currently on then turn all extensions off
 		chrome.storage.sync.get(function(obj){
 			Object.keys(obj).forEach(function(key){
 				if (key === btnIdConvert) {
 					for (var i = 0; i < obj[btnIdConvert].length; i++) {
-						chrome.management.setEnabled(obj[btnIdConvert][i], false, function (){
-								Materialize.toast(btnIdConvert+' is now ff', 2000, 'ccToastOff')
-							}
-						)
+						chrome.management.setEnabled(obj[btnIdConvert][i], false, function (){})
 					}
 				}
+				Materialize.toast(btnIdConvert+' is now off', 2000, 'ccToastOff')
 			})
 		})
-	$(this).removeClass("off").addClass("on");
+	$(this).removeClass("on").addClass("off"); // change profile btn to "off" appearance
+	location.reload(false); // adding false lets the page reload from the cache
 	}
 
 	else if ($(this).hasClass("off")) { // if the btn is currently off then turn all extensions on
@@ -147,46 +129,39 @@ $("body").on("click",".profile-btn",function(){ // if a profile btn is clicked
 			Object.keys(obj).forEach(function(key){
 				if (key === btnIdConvert) {
 					for (var i = 0; i < obj[btnIdConvert].length; i++) {
-						chrome.management.setEnabled(obj[btnIdConvert][i], true, function (){
-								Materialize.toast(btnIdConvert+' is now on', 2000, 'ccToastOn')
-							}
-						)
+						chrome.management.setEnabled(obj[btnIdConvert][i], true, function (){})
 					}
 				}
+				Materialize.toast(btnIdConvert+' is now on', 2000, 'ccToastOn')
 			})
 		})
-	$(this).removeClass("off").addClass("on");
+	$(this).removeClass("off").addClass("on"); // change profile btn to "on" appearance
+	location.reload(false); // adding false lets the page reload from the cache
 	}
 
 })
 
-
 function extStateListener() { // turn on/off extensions when toggle is switched
-	$('.js-switch').change(
-		function(){
-			var id = $(this).parents('.switch').attr('id'); // get the app id
-			var name = $(this).parents('.switch').attr('name'); // get the app name
-			if($(this).is(':checked')){
-				chrome.management.setEnabled(id, true, function (){
-					Materialize.toast(name+' is now on', 2000, 'ccToastOn');
-				});
-			} else {
-				chrome.management.setEnabled(id, false, function (){
-					Materialize.toast(name+' is now off', 2000, 'ccToastOff');
-				});
-			}
-		})
+	$('.js-switch').change(function(){
+		var id = $(this).parents('.switch').attr('id'); // get the app id
+		var name = $(this).parents('.switch').attr('name'); // get the app name
+		if($(this).is(':checked')){
+			chrome.management.setEnabled(id, true, function (){
+				Materialize.toast(name+' is now on', 2000, 'ccToastOn');
+			});
+		} else {
+			chrome.management.setEnabled(id, false, function (){
+				Materialize.toast(name+' is now off', 2000, 'ccToastOff');
+			});
+		}
+	})
 }
 
-
-// listen for addProfile button press, add a button to list, prompt for profile name, set that name as button text, add that profile to the storage.sync object
+// listen for addProfile button press, add a button to HTML, prompt for profile name, set that name as button text, add that profile to the storage.sync object
 $('#addProfile').click(
 	function(){
-		console.log('user is adding a profile');
-		//open modal
 		$('#profilePrompt').openModal({
-			complete: function() {
-				// function to run when modal is dismissed
+			complete: function() { // function to run when modal is dismissed
 			},
 		});
 	});
@@ -194,18 +169,16 @@ $('#addProfile').click(
 $('#nameSubmit').submit(
 	function(e){
 		e.preventDefault();
-		// catch the name the user selected
-		name = $('#name').val().toLowerCase();
-		// check if it's empty
-		// if (name === ""){
-		// 	// name is empty, don't close modal and prompt user for name
-		// 	console.log('name is empty, user must enter a (unique) name');
-		// 	Materialize.toast('Your profile needs a name!', 2000, 'alert');
+		name = $('#name').val().toLowerCase(); // catch the profile name the user entered
+		// // check if it's a valid name
+		// if (name ????){ // regex error
+		// // name doesn't match regex rules, don't close modal and prompt user for name
+		// 	Materialize.toast('Profile names must be between 1 and 15 characters and only include letters, numbers, and spaces', 3000, 'alert');
 		// 	return;
 		// }
 
-		// check if it's the same name as an existing profile
-		// if ($.inArray(name, profiles) != -1){
+		// // check if it's the same name as an existing profile
+		// if ($.inArray(name, profiles) != -1){ // schema has changed so this needs to change - check keys instead of profiles array
 		// 	console.log('profile already exists');
 		// 	Materialize.toast('Profile already exists!', 2000, 'alert');
 		// 	return;
@@ -214,76 +187,52 @@ $('#nameSubmit').submit(
 		console.log('user is adding the '+name+' profile');
 		// HTML code for profile btn changing string to lower case and replacing spaces with underscores
 		var btnHtml = "<a class='profile-btn off' id='"+name.split(' ').join('_')+"'>"+name+"</a>";
-		// append new button with new profile name to profile-holder
-		$('.profile-holder').append(btnHtml);
-		// set profile name to user-defined profile name
-		$('#name').val("");
-		// close modal
+		$('.profile-holder').append(btnHtml); // append new button with new profile name to profile-holder
+		$('#name').val(""); // set profile name to user-defined profile name
 		$('#profilePrompt').closeModal();
-
-		// after half a second open the modal, user can specify what extensions to add to profile
 		setTimeout(function(){
 			addExtensions(name);
-		}, 500)
+		}, 500) // after half a second open the modal, user can specify what extensions to add to profile
 	}
 )
 
-
-// add extensions to new profile modal
-var addExtensions = function(name){
-	// open the modal
+function addExtensions(name) { // add extensions to new profile modal
 	$('#addExts').openModal({
 		ready: function() {
 			checkboxlistener(name)
 		},
 		complete: function() {
-			// turn h4 text back to normal after modal is dismissed
-			$('#addExts h4').text(a);
+			$('#addExts h4').text(a); // turn h4 text back to normal after modal is dismissed
 			$('#extList').html('');
 			submitThatShit();
 		}
 	});
 
-	// change H4 text to say "add extensions to [profile name]"
 	var a = $('#addExts h4').text();
-	$('#addExts h4').text(a + " " + name);
-
-	// loop over extArray to populate the list
-	extArray.forEach(function(ext){
-		// console.log(ext);
+	$('#addExts h4').text(a + " " + name); // change H4 text to say "add extensions to [profile name]"
+	extArray.forEach(function(ext){ // loop over extArray to populate the list
 		$('#extList').append(extListTemplate(ext));
 	})
 }
 
-
 function checkboxlistener() { // turn on/off extensions when toggle is switched
-	$('.extList-toggle input').change(
-		function(){
-			//get the extension id the user clicked
-			var id = $(this).attr('appid');
-			idList.push(id)
-			console.log("single " + id)
-			console.log("all " + idList);
-		}
-	)
+	$('.extList-toggle input').change(function(){
+		var id = $(this).attr('appid'); //get the extension id the user clicked
+		idList.push(id)
+	})
 }
 
 function submitThatShit() {
 	tempObj = {};
-	console.log("done")
 	tempObj[name] = idList;
 	chrome.storage.sync.set(tempObj, function () {
 	  console.log('Saved', name, idList);
 	})
 }
 
-
-// retrieve profiles from chrome.storage:
-var getProfiles = function(){
-	// check storage for any profiles
+function getProfiles() { // check storage for any profiles
 	chrome.storage.sync.get(function(obj){
-		// if there are no profiles, exit function
-		if ( Object.keys(obj).length === 0 ) {
+		if ( Object.keys(obj).length === 0 ) { // if there are no profiles, exit function
 			$('#noProfilesText').show();
 			$('#profileHeader').css("background-color", "#03A9FA");
 			$('#editBtn, .profileBtn1, .profileBtn2, .profileBtn3, .profileBtn4, .profileBtn5').hide();
@@ -299,14 +248,12 @@ var getProfiles = function(){
 		for (var i = 0; i < allKeys.length; i++) {
 			var name = allKeys[i];
 			var btnHtml = "<a class='profile-btn off' id='"+name.toLowerCase()+"'>"+name+"</a>";
-			// append to profile-holder
-			$('.profile-holder').append(btnHtml);
+			$('.profile-holder').append(btnHtml); // append to profile-holder
 		};
 	})
 }
 
-
-// remove all profiles from chrome.storage:
+// temp button to remove all profiles from chrome.storage:
 $("#rmv").click(function(){
 	// a quick one-line removes all profiles
 	chrome.storage.sync.clear();
